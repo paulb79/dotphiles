f18b8be90f9fe42c8dda988c04abf1fc7ffd8feb
Paul Brown <2188950+paulb79@users.noreply.github.com>
2020-03-24 21:26:51 +0000

    Add brew packages and configure history

---
+++ brew-cask.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+# to maintain cask ....
+#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
+
+
+# Install native apps
+
+brew install caskroom/cask/brew-cask
+# brew tap caskroom/versions
+
+# brew cask install disk-inventory-x
+brew cask install 1password
+brew cask install alfred 
+brew cask install atom    
+brew cask install chai
+brew cask install chatmate-for-whatsapp                 
+brew cask install cheatsheet
+brew cask install daisydisk
+brew cask install dash
+brew cask install deezer
+brew cask install docker                   
+brew cask install dropbox
+brew cask install firefox                  
+brew cask install flux                     
+brew cask install gitup                    
+brew cask install google-chrome            
+brew cask install gpgtools
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install intellij-idea            
+brew cask install iterm2
+brew cask install karabiner-elements
+brew cask install mysqlworkbench           
+brew cask install postman
+brew cask install sequel-pro
+brew cask install setapp                   
+brew cask install slack
+brew cask install spectacle
+brew cask install sublime-text
+brew cask install superduper
+brew cask install torbrowser
+brew cask install utorrent
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install virtualbox
+brew cask install visual-studio-code
+brew cask install vlc
+brew cask install vnc-viewer
+
+
+brew tap caskroom/fonts 
+brew cask install font-fira-code
---
+++ brew.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Install command-line tools using Homebrew
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+brew install ansible
+brew install apache-spark
+brew install asciidoc
+brew install automake
+brew install bash
+brew install bash-completion
+brew install cabal-install
+brew install cask
+brew install clojure
+brew install coreutils
+brew install entr
+brew install ffmpeg
+brew install findutils
+brew install fzf
+brew install ghostscript
+brew install git
+brew install gnu-sed
+brew install google-authenticator-libpam
+brew install gradle
+brew install grep
+brew install groovy
+brew install homebrew/completions/brew-cask-completion
+brew install hub
+brew install imagemagick
+brew install jenv
+brew install jq
+brew install libksba
+brew install maven
+brew install moreutils
+brew install mtr
+brew install mysql
+brew install mysql-client
+brew install nano
+brew install ncdu # find where your diskspace went
+brew install node # This installs `npm` too using the recommended installation method
+brew install oath-toolkit
+brew install openssh
+brew install openvpn
+brew install packer
+brew install pidcat   # colored logcat guy
+brew install postgresql
+brew install pv
+brew install rename
+brew install rpm
+brew install sbt
+brew install scala
+brew install screen
+brew install sshpass
+brew install telnet
+brew install terminal-notifier
+brew install tesseract
+brew install the_silver_searcher
+brew install tmux
+brew install tor
+brew install tree
+brew install truncate
+brew install vim neovim 
+brew install watch
+brew install wget
+brew install wget
+brew install z
+brew install zlib
+brew install zopfli
+brew install zsh
+
+# allow mtr to run without sudo
+mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
+sudo chmod 4755 $mtrlocation/sbin/mtr
+sudo chown root $mtrlocation/sbin/mtr
+
+# Remove outdated versions from the cellar
+brew cleanup
